0be3560e64c7243686905a2d6f0f1e907263ad83
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-12-20 18:09:32 -0800

    removed old landing content code (animated text)

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -4,17 +4,6 @@ export default function Intro() {
   return (
     <div>
       <div className="flex flex-col text-center text-black pt-32 md:pt-48">
-        {/* <div className="text-custom-green group">
-          <div className=" font-montserrat text-5xl pt-8 pb-8 sm:text-7xl md:text-8xl lg:text-8xl group-hover:opacity-50 group-hover:scale-75 ease-in-out duration-700 delay-100">
-            Mentoring
-          </div>
-          <div className=" font-rock-salt text-5xl pb-8 sm:text-7xl md:text-8xl lg:text-9xl group-hover:scale-110 ease-in-out duration-700 delay-300">
-            Transforms
-          </div>
-          <div className=" font-montserrat text-5xl sm:text-7xl md:text-8xl lg:text-8xl group-hover:opacity-50 group-hover:scale-75 ease-in-out duration-700 delay-500">
-            Lives
-          </div>
-        </div> */}
         <div className="text-custom-green font-montserrat font-bold text-5xl sm:text-7xl md:text-8xl lg:text-8xl">
           <div className="pt-8 pb-8">Mentoring</div>
           <div className="font-rock-salt text-5xl sm:text-7xl md:text-8xl lg:text-9xl pb-8">
