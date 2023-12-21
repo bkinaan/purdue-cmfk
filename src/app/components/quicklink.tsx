702b6f335354ec05e77f02b82a34e567e89a4532
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-12-20 18:09:59 -0800

    added white background to text

--- src/app/components/quicklink.tsx
+++ src/app/components/quicklink.tsx
@@ -1,7 +1,7 @@
 export default function Quicklink(props: { title: string; link: string }) {
   return (
     <a href={props.link} className="relative inline-block text-4xl group">
-      <button className="border-2 font-normal border-custom-green rounded-full p-4 font-montserrat shadow-lg shadow-custom-green hover:bg-custom-green hover:translate-y-1 ease-in-out duration-200">
+      <button className="bg-white border-2 font-normal border-custom-green rounded-full p-4 font-montserrat shadow-lg shadow-custom-green hover:bg-custom-green hover:translate-y-1 ease-in-out duration-200">
         {props.title}
       </button>
     </a>
