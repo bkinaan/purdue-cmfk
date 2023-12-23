2f13e3157d96e34162090df9039f45dee27e06d5
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-12-22 18:15:53 -0800

    updated with more accurate text and flex

--- src/app/components/fundraising.tsx
+++ src/app/components/fundraising.tsx
@@ -2,18 +2,25 @@ import Iframe from "react-iframe";
 
 export default function Fundraising() {
   return (
-    <div className="grid grid-cols-2 items-center pt-8">
-      <div className="font-montserrat text-5xl pb-16">
-        Fundraising
-        <div className="text-xl">Test</div>
+    <div className="flex flex-row items-center justify-center flex-wrap md:space-x-32">
+      <div className="flex flex-col justify-center font-montserrat text-5xl pb-16 text-white">
+        <div className="flex justify-center pb-4">Fundraising</div>
+        <div className="flex text-lg mx-auto justify-center max-w-lg">
+          Every mentor at College Mentors for Kids plays a crucial role not only
+          in guiding our little buddies but also in ensuring the sustainability
+          of our program. As part of their commitment, each mentor is required
+          to meet an individual fundraising goal. The funds raised directly
+          contribute to our programming expenses, helping us transform more
+          lives through mentoring.
+        </div>
       </div>
 
-      <div className="flex justify-center">
+      <div className="flex flex-row items-center justify-center flex-wrap pb-32">
         <Iframe
           url="https://secure.qgiv.com/event/collegementorspurdue/widget/6546786/?etype=event&entity=1009365"
           width="272.664px"
           height="446.742px"
-          className="bg-white rounded-xl shadow-xl shadow-custom-green mb-16 p-8"
+          className="bg-white rounded-xl shadow-lg shadow-gray-500 mb-16 p-8"
           scrolling="no"
         />
       </div>
